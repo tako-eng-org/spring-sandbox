aabebd8c28d3ed5061884da86f950048a56c449e
tako_eng <oys.sys@gmail.com>
2020-09-13 00:54:43 +0000

    Controllerのファイル名を変更した

--- app/src/main/java/com/example/demo/controller/HomeController.java
+++ app/src/main/java/com/example/demo/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class HomeController {
 	@Autowired
 	UserRepository userRepository;
 

f2515836974a400526d581850165fd0a1c51350d
tako_eng <oys.sys@gmail.com>
2020-09-13 00:56:27 +0000

    AbstractModelを作成した

--- app/src/main/java/com/example/demo/jpa/AbstractModel.java
+++ app/src/main/java/com/example/demo/jpa/AbstractModel.java
@@ -11,16 +11,18 @@ import lombok.Getter;
 import lombok.Setter;
 import java.sql.Timestamp;
 
-// 日時情報など、1テーブルごとに必要になるような情報
+// 日時情報など、1テーブルごとに共通して必要になるようなカラムを作成するモデル
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter // lombokによる
 @Setter // lombokによる
 public abstract class AbstractModel {
+  // 作成日時のカラムを作成する
   @CreatedDate
   @Column(nullable = false)
   private Timestamp createdDate;
 
+  // 更新日時のカラムを作成する
   @LastModifiedDate
   @Column(nullable = false)
   private Timestamp lastModifiedDate;
